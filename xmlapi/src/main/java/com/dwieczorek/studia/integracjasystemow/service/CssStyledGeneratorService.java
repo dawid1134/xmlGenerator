3e94d3c277b37315b3cf162cb6354e0124baa709
dawid1134 <wieczorek.dawid@outlook.com>
2017-01-01 16:03:05 +0100

    [0.0.8] - Implement parser selector

--- xmlapi/src/main/java/com/dwieczorek/studia/integracjasystemow/service/CssStyledGeneratorService.java
+++ xmlapi/src/main/java/com/dwieczorek/studia/integracjasystemow/service/CssStyledGeneratorService.java
@@ -2,8 +2,10 @@ package com.dwieczorek.studia.integracjasystemow.service;
 
 import com.dwieczorek.studia.integracjasystemow.converter.ApiParser;
 import com.dwieczorek.studia.integracjasystemow.converter.ServerSupportedType;
+import com.dwieczorek.studia.integracjasystemow.converter.parser.XmlApiParser;
 import com.dwieczorek.studia.integracjasystemow.dao.dto.CustomerData;
 import com.dwieczorek.studia.integracjasystemow.utils.XmlList;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,15 +13,20 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by dawid on 01.01.2017.
  */
+@Service
 public class CssStyledGeneratorService extends ResponseGeneratorService {
 
     @Override
-    protected void generateResponse(HttpServletRequest request, HttpServletResponse response, ApiParser<XmlList<CustomerData>> apiParser, XmlList<CustomerData> xmlList) {
+    protected void generateResponse(HttpServletRequest request, HttpServletResponse response,
+                                    ApiParser<XmlList<CustomerData>> apiParser, XmlList<CustomerData> xmlList) {
 
     }
 
     @Override
     protected ApiParser<XmlList<CustomerData>> createProperParser(ServerSupportedType type) {
-        return null;
+        if (type == ServerSupportedType.XML) {
+            return new XmlApiParser<>();
+        }
+        throw new UnsupportedOperationException("CSS Styled generator service support only XML format.");
     }
 }
